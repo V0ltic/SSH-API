817c68fe3933f0260219568f3cf06f374672f0f6
V0ltic <voltic-support@protonmail.com>
2021-02-25 19:16:40 +0100

    API.php

---
+++ API.php
@@ -0,0 +1,101 @@
+<?php
+    // Usage: https://server_ip/API.php?pass=yourpassword
+   
+
+    // Print out any errors that occur
+
+    error_reporting(E_ALL);
+
+
+
+
+    // SSH Info
+
+    $ip = "";
+
+    $port = "22";
+
+    $ssh_pass = "";
+
+    $ssh_username = "root";
+
+
+
+
+    // Encode special characters
+
+    function EncodeString($string)
+    {
+        return htmlspecialchars($string, ENT_QUOTES, "UTF-8");
+    }
+ 
+    
+
+
+    // Check if password has been submitted
+
+    $pass = array("password");
+ 
+    if (!isset($_GET["pass"]))
+    {
+        die("Please submit a password!");
+    }
+ 
+
+
+    // Variables
+ 
+    $ssh = ""; // Your command goes here
+
+    $password = EncodeString($_GET["pass"]); // Getting the password in the GET request
+ 
+ 
+ 
+
+    // Check if given password is right
+ 
+    if (!in_array($password, $pass)){
+        die("Invalid Password!");
+    }
+ 
+
+    
+ 
+    // Connect to the server
+ 
+    $socket = fsockopen($ip, $port);
+ 
+    ($socket ? null : die("Failed to connect"));
+ 
+    fwrite($socket, " \r\n");
+   
+    sleep(3);
+   
+    fwrite($socket, $ssh_username . "\r\n");
+   
+    sleep(3);
+   
+    fwrite($socket, $ssh_pass . "\r\n");
+ 
+ 
+
+
+    // Send command
+ 
+    sleep(9);
+ 
+    fwrite($socket, $ssh);
+ 
+
+
+    // Close connection
+ 
+    fclose($socket);
+ 
+
+    // Notify the command went through
+ 
+    echo "Your command has been successfully send to the server!";
+ 
+   
+?>
